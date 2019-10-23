e60efae38080c6dbfe9333f213a023850112588e
DeaconRodda <deacon@sqglz.com>
2019-10-22 21:10:10 -0600

    all articles added. Linking between suggestions and articles works. We're looking pretty good.

--- scripts/contact.js
+++ scripts/contact.js
@@ -97,7 +97,7 @@
 	]
 
 	const articles = [
-		{title: "Reprogram the Subconscious while Asleep", summary: "Bruce Lipton, with special guest Niel Gaiman, explains the methods for accessing dreams with nothing but a spellbook bound in human skin and a handful of sand.", link:"URL",
+		{title: "Reprogram the Subconscious while Asleep", summary: "Bruce Lipton, with special guest Niel Gaiman, explains the methods for accessing dreams with nothing but a spellbook bound in human skin and a handful of sand.", link:"./h/reprogram-the-subconscious-while-asleep.html",
 		tags: [
 			{tag: "beliefchange", value: 9},
 			{tag: "belief", value: 9},
@@ -106,7 +106,7 @@
 		rank: 0
 		},
 
-		{title: "Updating Your Payment Details", summary: "In this video Deacon will be walking you through the steps taken to change your card on file, cancel your account, and more.", link:"URL",
+		{title: "Updating Your Payment Details", summary: "In this video Deacon will be walking you through the steps taken to change your card on file, cancel your account, and more.", link:"./h/update-payment-details.html",
 		tags: [
 			{tag: "accountmgmt", value: 9},
 			{tag: "finances", value: 3},
@@ -115,7 +115,7 @@
 		rank: 0
 		},
 
-		{title: "Inviting Bruce to Speak at Your Event", summary: "Bruce Lipton loves to engage intimately with his audience. If you're hosting an event, this article will help you determine whether Bruce Lipton is a good fit, and the steps involved in making that connection.", link:"URL",
+		{title: "Inviting Bruce to Speak at Your Event", summary: "Bruce Lipton loves to engage intimately with his audience. If you're hosting an event, this article will help you determine whether Bruce Lipton is a good fit, and the steps involved in making that connection.", link:"./h/invite-bruce.html",
 		tags: [
 			{tag: "events", value: 8},
 			{tag: "invitebruce", value: 9},
@@ -125,7 +125,7 @@
 		rank: 0
 		},
 
-		{title: "Stream Bruce's Content On the Go", summary: "Get the most out of your commute, your household tasks, or your workout time by listening to Bruce when it's most convenient.", link:"URL",
+		{title: "Stream Bruce's Content On the Go", summary: "Get the most out of your commute, your household tasks, or your workout time by listening to Bruce when it's most convenient.", link:"./h/stream-content.html",
 		tags: [
 			{tag: "lifestyle", value: 7},
 			{tag: "support", value: 4},
@@ -134,7 +134,7 @@
 		rank: 0
 		},
 
-		{title: "Find Your Previous Purchases", summary: "Downloads, streaming content and all other previous purchases are still available. This video shows you how to find them.", link:"URL",
+		{title: "Find Your Previous Purchases", summary: "Downloads, streaming content and all other previous purchases are still available. This video shows you how to find them.", link:"./h/find-previous-purchase.html",
 		tags: [
 			{tag: "support", value: 7},
 			{tag: "purchases", value: 8},
@@ -143,7 +143,7 @@
 		rank: 0
 		},
 
-		{title: "Self-Limiting Beliefs", summary: "Understand what makes a belief self-limiting, where they come from, and the reasons they're so hard to change", link:"URL",
+		{title: "Self-Limiting Beliefs", summary: "Understand what makes a belief self-limiting, where they come from, and the reasons they're so hard to change", link:"./h/self-limiting-beliefs.html",
 		tags: [
 			{tag: "belief", value: 9},
 			{tag: "beliefchange", value: 7},
@@ -152,9 +152,9 @@
 		rank: 0
 		},
 
-		{title: "Overview of Belief Change Modalities", summary: "There are many modalities of belief change available. This article reviews several to help you determine which is right for you.", link:"URL",
+		{title: "Overview of Belief Change Modalities", summary: "There are many modalities of belief change available. This article reviews several to help you determine which is right for you.", link:"./h/overview-of-belief-change-modalities.html",
 		tags: [
-			{tag: "beliefchange", value: 9},
+			{tag: "beliefchange", value: 6},
 			{tag: "belief", value: 6},
 			{tag: "habits", value: 4},
 			{tag: "childhood", value: 2},
@@ -163,7 +163,7 @@
 		rank: 0
 		},
 
-		{title: "Getting Started with Psych-K", summary: "Psych-K is a popular method for bringing balance to unproductive beliefs. In this article Rob Williams explains how to get started.", link:"URL",
+		{title: "Getting Started with Psych-K", summary: "Psych-K is a popular method for bringing balance to unproductive beliefs. In this article Rob Williams explains how to get started.", link:"./h/getting-started-with-psych-k.html",
 		tags: [
 			{tag: "psychk", value: 9},
 			{tag: "beliefchange", value: 5},
@@ -172,7 +172,7 @@
 		rank: 0
 		},
 
-		{title: "Financial and Geographic Limitations and Psych-K Treatment", summary: "Rob Williams gives advice to seekers finding it difficult to attend Psych-K events.", link:"URL",
+		{title: "Financial and Geographic Limitations and Psych-K Treatment", summary: "Rob Williams gives advice to seekers finding it difficult to attend Psych-K events.", link:"./h/financial-limitations-and-psych-k.html",
 		tags: [
 			{tag: "psychk", value: 8},
 			{tag: "lifestyle", value: 3},
@@ -182,17 +182,17 @@
 		rank: 0
 		},
 
-		{title: "Community Support for Belief Change", summary: "Premium membership to brucelipton.com is a fantastic way to find real human support on your journey to a renewed subconscious.", link:"URL",
+		{title: "Community Support for Belief Change", summary: "Premium membership to brucelipton.com is a fantastic way to find real human support on your journey to a renewed subconscious.", link:"./h/community-support-for-belief-change.html",
 		tags: [
-			{tag: "community", value: 7},
+			{tag: "community", value: 9},
 			{tag: "beliefchange", value: 6},
-			{tag: "lifestyle", value: 3},
-			{tag: "habits", value: 5}
+			{tag: "lifestyle", value: 9},
+			{tag: "habits", value: 9}
 			],
 		rank: 0
 		},
 
-		{title: "Premium Membership Details", summary: "Learn about all of the features and benefits of premium membership here at brucelipton.com.", link:"URL",
+		{title: "Premium Membership Details", summary: "Learn about all of the features and benefits of premium membership here at brucelipton.com.", link:"./h/premium-membership-details.html",
 		tags: [
 			{tag: "community", value: 5},
 			{tag: "lifestyle", value: 4},
@@ -202,7 +202,7 @@
 		rank: 0
 		},
 
-		{title: "Seek Immediate Help", summary: "Some concerns require that a seeker reach out for help through faster channels than we may be able to provide", link:"URL",
+		{title: "Seek Immediate Help", summary: "Some concerns require that a seeker reach out for help through faster channels than we may be able to provide", link:"./h/seek-immediate-help.html",
 		tags: [
 			{tag: "emergency", value: 9},
 			{tag: "treatment", value: 7},
@@ -425,6 +425,12 @@
 				let suggestionBoxThree = document.getElementById("suggestion-three");
 				let suggestionBoxFour = document.getElementById("suggestion-four");
 
+				let suggestionLinkOne = document.getElementById("suggestion-one-link");
+				let suggestionLinkTwo = document.getElementById("suggestion-two-link");
+				let suggestionLinkThree = document.getElementById("suggestion-three-link");
+				let suggestionLinkFour = document.getElementById("suggestion-four-link");
+
+
 				let rankedArticles = articles.sort(function(a1, a2) {
 					if(a1.rank > a2.rank) {
 						return -1;
@@ -443,24 +449,28 @@
 					'<p class="contact__suggestion-intro">' +
 					rankedArticles[0].summary	+
 					'</p>';
+				suggestionLinkOne.href = rankedArticles[0].link;
 
-					suggestionBoxTwo.innerHTML = '<h4 class="contact__suggestion-title">' + rankedArticles[1].title +
+				suggestionBoxTwo.innerHTML = '<h4 class="contact__suggestion-title">' + rankedArticles[1].title +
 					'</h4>' +
 					'<p class="contact__suggestion-intro">' +
 					rankedArticles[1].summary	+
 					'</p>';
+				suggestionLinkTwo.href = rankedArticles[1].link;
 
-					suggestionBoxThree.innerHTML = '<h4 class="contact__suggestion-title">' + rankedArticles[2].title +
+				suggestionBoxThree.innerHTML = '<h4 class="contact__suggestion-title">' + rankedArticles[2].title +
 					'</h4>' +
 					'<p class="contact__suggestion-intro">' +
 					rankedArticles[2].summary	+
 					'</p>';
+				suggestionLinkThree.href = rankedArticles[3].link;
 
-					suggestionBoxFour.innerHTML = '<h4 class="contact__suggestion-title">' + rankedArticles[3].title +
+				suggestionBoxFour.innerHTML = '<h4 class="contact__suggestion-title">' + rankedArticles[3].title +
 					'</h4>' +
 					'<p class="contact__suggestion-intro">' +
 					rankedArticles[3].summary	+
 					'</p>';
+				suggestionLinkFour.href = rankedArticles[3].link;
 
 		}
 
